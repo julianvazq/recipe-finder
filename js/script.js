3468972ad9b604a8a53fee5e465ecd0a74753830
julianvazq <50368117+julianvazq@users.noreply.github.com>
2019-07-18 15:01:22 -0400

    Update functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-//DOM Elements
-let imageElements = document.querySelectorAll("img");
-let titleElements = document.querySelectorAll("h3");
-
 let ingredients = [];
 
 function requestData(e) {
@@ -11,8 +7,14 @@ function requestData(e) {
 
   var imagesURL = [];
   var titles = [];
+  var sourceURL = [];
 
-  addIngredients();
+  if (!addIngredients()) {
+    return;
+  }
+
+  removeExistingElements();
+
   let url =
     "https://www.food2fork.com/api/search?key=19d852284c0af6e8548dbb3aff623650&q=";
 
@@ -27,44 +29,109 @@ function requestData(e) {
   xmlhttp.onreadystatechange = () => {
     if (xmlhttp.readyState === xmlhttp.DONE && xmlhttp.status === 200) {
       var myObj = JSON.parse(xmlhttp.responseText);
+      var results = document.querySelector("#results");
       if (myObj.count === 0) {
+        results.innerHTML = 0;
         alert("Sorry, no recipes with these ingredients.");
         return 0;
       }
-      console.log(myObj);
+      results.innerHTML = myObj.count;
       for (let i = 0; i < myObj.count; i++) {
         //Store image_url property in array
         if (myObj.recipes[i].hasOwnProperty("image_url")) {
           imagesURL.push(myObj.recipes[i].image_url);
           titles.push(myObj.recipes[i].title);
+          sourceURL.push(myObj.recipes[i].source_url);
         }
       }
+      // Populate images and titles
+      populate(myObj.count, imagesURL, titles, sourceURL);
     } else {
       alert("There was a problem with the request.");
     }
   };
 
   xmlhttp.send();
+}
 
-  // Populate images and titles
-  for (let i = 0; i < imageElements.length; i++) {
-    imageElements[i].src = imagesURL[i];
-    titleElements[i].innerHTML = titles[i];
+function populate(count, imagesURL, titles, sourceURL) {
+  for (let i = 0; i < count; i++) {
+    //Create elements and add classes
+    var li = document.createElement("li");
+    var anchor = document.createElement("a");
+    anchor.className = "card";
+    var figure = document.createElement("figure");
+    var img = document.createElement("img");
+    var figCaption = document.createElement("figcaption");
+    figCaption.className = "caption";
+    var title = document.createElement("h3");
+    title.className = "caption";
+
+    //Populate elements
+    img.src = imagesURL[i];
+    title.innerHTML = titles[i];
+    anchor.href = sourceURL[i];
+
+    //Add elements to document
+    figCaption.appendChild(title);
+    figure.appendChild(img);
+    figure.appendChild(figCaption);
+    li.appendChild(figure);
+    anchor.appendChild(li);
+    document.querySelector(".cards").appendChild(anchor);
   }
 
-  //Reset ingredients array
+  //Show entered ingredients and reset ingredients array
+  let ingredientDisplay = document.querySelector("#display-ingredients");
+  ingredientDisplay.innerHTML = "Ingredients: ";
+  for (let i = 0; i < ingredients.length; i++) {
+    ingredientDisplay.innerHTML += ingredients[i];
+    if (ingredients.length > 1 && i < ingredients.length - 1) {
+      ingredientDisplay.innerHTML += ", ";
+    }
+  }
   ingredients = [];
 }
 
-function addIngredients() {
-  if (ingredientInput.length != 0) {
-    let ingredientInput = document.querySelector("#ingredientInput").value;
-    ingredients.push(ingredientInput);
-    // console.log(ingredientInput);
-    document.querySelector("form").reset();
-    // console.log(ingredients);
+function removeExistingElements() {
+  let ingredientList = document.querySelectorAll(".ingredient-list li");
+  for (let i = 0; i < ingredientList.length; i++) {
+    document.querySelector(".ingredient-list").removeChild(ingredientList[i]);
+  }
+
+  let list = document.querySelector(".cards");
+  let anchors = document.querySelectorAll(".card");
+  for (let i = 0; i < anchors.length; i++) {
+    list.removeChild(anchors[i]);
+  }
+}
+
+function addIngredients(e) {
+  let ingredientInput = document.querySelector("#ingredientInput").value;
+  if (ingredientInput.length > 0) {
+    //IF input not empty, push to array and return true
+
+    if (!ingredients.includes(ingredientInput)) {
+      ingredients.push(ingredientInput);
+      document.querySelector("form").reset();
+      let ingredientElem = document.createElement("li");
+      ingredientElem.innerHTML = ingredientInput;
+      document.querySelector(".ingredient-list").appendChild(ingredientElem);
+    } else {
+      alert("Ingredient you are trying to add is already in the list.");
+    }
+
+    return true;
+  } else if (ingredientInput.length == 0 && ingredients.length > 0) {
+    //If user try to add with empty input, return false
+    if (e != undefined) {
+      alert("Please enter an ingredient.");
+      return false;
+    }
+    return true;
   } else {
     alert("Please enter an ingredient.");
+    return false;
   }
 }
 
