b32062141f082b7a8d6e3b73bd39d25d549436fd
julianvazq <50368117+julianvazq@users.noreply.github.com>
2019-08-03 14:41:42 -0400

    Minor styling changes

--- js/script.js
+++ js/script.js
@@ -130,7 +130,7 @@ function addIngredients(e) {
   if (ingredientInput.length > 0) {
     if (!allLetter(ingredientInput)) {
       return false;
-    } 
+    }
     //IF input not empty, push to array and return true
     if (!ingredients.includes(ingredientInput)) {
       ingredients.push(ingredientInput);
@@ -144,7 +144,7 @@ function addIngredients(e) {
     } else {
       document.querySelector("#ingredientInput").value = "";
       alert("Ingredient you are trying to add is already in the list.");
-    } 
+    }
     return true;
   } else if (ingredientInput.length == 0 && ingredients.length > 0) {
     //If user try to add with empty input, return false
@@ -159,19 +159,17 @@ function addIngredients(e) {
   }
 }
 
-function allLetter(inputtxt)
-  {
-   var letters = /^[a-zA-Z ]+$/;
-   if(inputtxt.match(letters))
-     {
-      return true;
-     }
-   else
-     {
-     alert("Please enter a valid ingredient (no numbers or special characters).");
-     return 0;
-     }
+function allLetter(inputtxt) {
+  var letters = /^[a-zA-Z ]+$/;
+  if (inputtxt.match(letters)) {
+    return true;
+  } else {
+    alert(
+      "Please enter a valid ingredient (no numbers or special characters)."
+    );
+    return 0;
   }
+}
 
 function deleteIngredient(e) {
   e.preventDefault();
@@ -191,6 +189,7 @@ function deleteIngredient(e) {
   }
 }
 
+//Event Listeners
 document.querySelector("#submit").addEventListener("click", requestData);
 document.querySelector("#add").addEventListener("click", addIngredients);
 document.querySelector("#clear").addEventListener("click", () => {
